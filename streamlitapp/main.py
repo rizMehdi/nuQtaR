e72a923c10967778f19a7d34039a77348a61c1db
rizMehdi <ccmeall@gmail.com>
2024-01-01 13:51:55 +0000

    import moduledrawers

--- streamlitapp/main.py
+++ streamlitapp/main.py
@@ -8,7 +8,7 @@ from qrcode.image.styledpil import StyledPilImage
 # from qrcode.image.styles.moduledrawers.pil import GappedSquareModuleDrawer, CircleModuleDrawer, RoundedModuleDrawer, VerticalBarsDrawer, HorizontalBarsDrawer, SquareModuleDrawer
 # ^mistake in lib\
 
-import qrcode.image.styles
+import qrcode.image.styles.moduledrawers
 
 
 from qrcode.image.styles.colormasks import RadialGradiantColorMask
